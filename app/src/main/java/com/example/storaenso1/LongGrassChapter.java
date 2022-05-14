c156f4158ec637aeac84cd01d7b493c086c8f9eb
sherlockHolmes <sherloxk5254@gmail.com>
2022-05-14 21:03:28 +0800

    長草ONE

--- app/src/main/java/com/example/storaenso1/LongGrassChapter.java
+++ app/src/main/java/com/example/storaenso1/LongGrassChapter.java
@@ -3,14 +3,28 @@ package com.example.storaenso1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LongGrassChapter extends AppCompatActivity {
+
+    ImageView bag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_long_grass_chapter);
 
+        init();
+    }
 
+    public void init(){
+        bag = (ImageView) findViewById(R.id.LongGrassBag);
+        bag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
